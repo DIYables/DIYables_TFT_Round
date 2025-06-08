9eb9f97037476f812b4dff0ef2fd6ad1683cf7c3
DIYables <134663277+DIYables@users.noreply.github.com>
2025-06-08 16:23:59 +0900

    Update ShowTextAndNumber.ino

--- examples/ShowTextAndNumber/ShowTextAndNumber.ino
+++ examples/ShowTextAndNumber/ShowTextAndNumber.ino
@@ -10,17 +10,18 @@
 
 #include <DIYables_TFT_Round.h>
 
-#define MAGENTA   DIYables_TFT::colorRGB(255, 0, 255)
-#define WHITE     DIYables_TFT::colorRGB(255, 255, 255)
+#define RED DIYables_TFT::colorRGB(255, 0, 0)
+#define BLUE DIYables_TFT::colorRGB(0, 0, 255)
+#define WHITE DIYables_TFT::colorRGB(255, 255, 255)
 
-#define PIN_RST 8
-#define PIN_DC  9
-#define PIN_CS  10
+#define PIN_RST 8  // The Arduino pin connected to the RST pin of the circular TFT display
+#define PIN_DC 9   // The Arduino pin connected to the DC pin of the circular TFT display
+#define PIN_CS 10  // The Arduino pin connected to the CS pin of the circular TFT display
 
 DIYables_TFT_GC9A01_Round TFT_display(PIN_RST, PIN_DC, PIN_CS);
 
 void setup() {
-  Serial.println(F("Arduino TFT LCD Display - show text and float number"));
+  Serial.println(F("Arduino TFT LCD Display - show text and number"));
 
   TFT_display.begin();
 
@@ -28,25 +29,25 @@ void setup() {
   TFT_display.setRotation(1);  // Rotate screen 90 degrees
   TFT_display.fillScreen(WHITE);
 
-  // Set text color and size
-  TFT_display.setTextColor(MAGENTA);
-  TFT_display.setTextSize(3);  // Adjust text size as needed
+  TFT_display.setTextSize(2);  // Adjust text size as needed
 
   // Sample temperature value
-  float temperature = 23.5;
-  float humidity = 78.6;
+  float temperature = 26.4;
+  float humidity = 64.7;
 
   // Display temperature with degree symbol
-  TFT_display.setCursor(20, 20);    // Set cursor position (x, y)
+  TFT_display.setTextColor(RED);
+  TFT_display.setCursor(5, 100);  // Set cursor position (x, y)
   TFT_display.print("Temperature: ");
   TFT_display.print(temperature, 1);  // Print temperature with 1 decimal place
   TFT_display.print(char(247));
   TFT_display.println("C");
 
   // Display humidity
-  TFT_display.setCursor(20, 60);    // Set cursor position (x, y)
+  TFT_display.setTextColor(BLUE);
+  TFT_display.setCursor(30, 140);  // Set cursor position (x, y)
   TFT_display.print("Humidity: ");
-  TFT_display.print(humidity, 1);   // Print humidity with 1 decimal place
+  TFT_display.print(humidity, 1);  // Print humidity with 1 decimal place
   TFT_display.print("%");
 }
 
